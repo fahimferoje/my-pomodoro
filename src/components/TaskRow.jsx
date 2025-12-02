5ea6503b864f1a15e1ea606149905f4214183dc6
fahimferoje <fabrahamjohn@gmail.com>
2025-12-02 13:08:05 +0600

    UI glitch fixed

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -16,16 +16,26 @@ const TaskRow = ({
         <div className="flex bg-white/80 rounded-lg w-md h-16 pl-2 items-center">
           <FontAwesomeIcon
             onClick={onTaskCheck}
-            className={`text-2xl ${iconBgColor}`}
+            className={`cursor-pointer text-2xl ${iconBgColor}`}
             icon={faCircleCheck}
           />
-          <div className={`font-semibold pl-2 basis-2/3`}>
-            <button className={textDecoration} onClick={onTaskNameClick}>
+          <div className={`font-semibold pl-2 basis-3xl cursor-pointer`}>
+            <button
+              className={`text-left cursor-pointer w-full ${textDecoration}`}
+              onClick={onTaskNameClick}
+            >
               {task.name}
             </button>
           </div>
-          <div className="basis-1/3">
-            <FontAwesomeIcon icon={faEllipsisVertical} />
+          <div className="basis-24 font-semibold mr-3">
+            <h1 className="text-center">0/3</h1>
+          </div>
+          <div
+            className="w-30 bg-white text-center 
+          basis-12 border-1 border-[#dfdfdf] mr-3 
+          cursor-pointer hover:bg-[#dfdfdf]"
+          >
+            <FontAwesomeIcon className="" icon={faEllipsisVertical} />
           </div>
         </div>
       ))}
